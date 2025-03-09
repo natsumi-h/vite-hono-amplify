ac521fe0c2ce0e5d237dfc1092f6318bfea4d729
Natsumi.H <natsmy.1211@gmail.com>
2025-03-09 12:40:31 +0900

    mod postbuild.sh

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -8,5 +8,7 @@ cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
 cp -r public ./.amplify-hosting/static
+cp -r ./dist/assets ./.amplify-hosting/static/assets
+cp -r ./dist/index.html ./.amplify-hosting/static/index.html
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
